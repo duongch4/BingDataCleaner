320a8ca644cd5b2bac8b214179d7de24b48b71e2
g2minhle <lehoangminh@live.com>
2016-08-25 14:58:58 -0400

    Add API end points to job controller and reduce number of instance

--- src/main/java/com/g2minhle/bingdatacleaner/controller/JobController.java
+++ src/main/java/com/g2minhle/bingdatacleaner/controller/JobController.java
@@ -11,8 +11,32 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class JobController {
 
 	@ResponseBody
-	@RequestMapping(value = "/{jobID}/", method = RequestMethod.GET)
-	public String getJobInformation(@PathVariable("jobID") String jobID) {
-		return "This is job: " + jobID;
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String createJob(
+			@PathVariable("userEmail") String userEmail,
+			@PathVariable("documentURL") String documentURL) {
+		return "Create job for user " + userEmail + " with document url: " + documentURL;
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/{jobID}", method = RequestMethod.GET)
+	public String getJobInformation(String jobID) {
+		return "This is information of job: " + jobID;
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/{jobID}", method = RequestMethod.PATCH)
+	public String updateJobInformation(
+			@PathVariable("jobID") String jobID,
+			String userEmail) {
+		return "Updated user email for job " + jobID + " to " + userEmail;
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/{jobID}/action", method = RequestMethod.POST)
+	public String performAnActionOnJob(
+			@PathVariable("jobID") String jobID,
+			String action) {
+		return "This is information of job: " + jobID;
 	}
 }

aa07221d3aed42c6d1b54e641c1622f1542dea3c
g2minhle <lehoangminh@live.com>
2016-09-07 22:32:21 -0400

    Update DocumentServices
    + Add writeToDocument
    + Add readFromDocument

--- src/main/java/com/g2minhle/bingdatacleaner/services/DocumentServices.java
+++ src/main/java/com/g2minhle/bingdatacleaner/services/DocumentServices.java
@@ -1,5 +1,7 @@
 package com.g2minhle.bingdatacleaner.services;
 
+import java.util.List;
+
 import com.g2minhle.bingdatacleaner.exception.CannotAccessToDocumentException;
 import com.g2minhle.bingdatacleaner.exception.DocumentServiceConnectivityException;
 import com.g2minhle.bingdatacleaner.exception.InvalidDocumentUrlException;
@@ -14,4 +16,13 @@ public interface DocumentServices {
 
 	public String getDocumentIdFromUrl(String documentUrl)
 			throws InvalidDocumentUrlException;
+
+	public List<String> readFromDocument(String documentId, Long start, Long count)
+			throws DocumentServiceConnectivityException;
+
+	public void writeToDocument(
+			String documentId,
+			Long start,
+			List<String> dataEntries,
+			List<String> searchResults) throws DocumentServiceConnectivityException;
 }
